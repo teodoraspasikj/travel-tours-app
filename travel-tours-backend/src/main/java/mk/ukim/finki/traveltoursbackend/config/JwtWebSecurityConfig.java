122508d1e6f1109d7379de70bf4ef3ab8dbca034
teodoraspasikj <spasikteodora85@gmail.com>
2026-09-19 21:17:58 +0200

    Split backend ConfigMap and Secret into separate manifests

--- travel-tours-backend/src/main/java/mk/ukim/finki/traveltoursbackend/config/JwtWebSecurityConfig.java
+++ travel-tours-backend/src/main/java/mk/ukim/finki/traveltoursbackend/config/JwtWebSecurityConfig.java
@@ -1,3 +1,93 @@
+//package mk.ukim.finki.traveltoursbackend.config;
+//
+//import java.util.List;
+//import mk.ukim.finki.traveltoursbackend.web.filter.JwtFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+//import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+//import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+//import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//public class JwtWebSecurityConfig {
+//    private final JwtFilter jwtFilter;
+//
+//    public JwtWebSecurityConfig(JwtFilter jwtFilter) {
+//        this.jwtFilter = jwtFilter;
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001"));
+//        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+//        corsConfiguration.setAllowedHeaders(List.of("*"));
+//        corsConfiguration.setAllowCredentials(true);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfiguration);
+//        return source;
+//    }
+//
+//    @Bean
+//    public RoleHierarchy roleHierarchy() {
+//        return RoleHierarchyImpl
+//            .withDefaultRolePrefix()
+//            .role("ADMINISTRATOR").implies("GUIDE")
+//            .role("GUIDE").implies("CUSTOMER")
+//            .build();
+//    }
+//
+//    @Bean
+//    static MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+//        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+//        expressionHandler.setRoleHierarchy(roleHierarchy);
+//        return expressionHandler;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//            .csrf(AbstractHttpConfigurer::disable)
+//            .cors(corsCustomizer ->
+//                corsCustomizer.configurationSource(corsConfigurationSource())
+//            )
+//            .headers(headers -> headers
+//                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+//            )
+//            .authorizeHttpRequests(authorizeHttpRequestsCustomizer ->
+//                authorizeHttpRequestsCustomizer
+//                    .requestMatchers(
+//                        "/swagger-ui/**",
+//                        "/v3/api-docs/**",
+//                        "/h2-console/**",
+//                        "/api/user/register",
+//                        "/api/user/login"
+//                    )
+//                    .permitAll()
+//                    .anyRequest()
+//                    .authenticated()
+//            )
+//            .sessionManagement(sessionManagementConfigurer ->
+//                sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//            )
+//            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+//        return http.build();
+//    }
+//}
 package mk.ukim.finki.traveltoursbackend.config;
 
 import java.util.List;
@@ -45,10 +135,10 @@ public class JwtWebSecurityConfig {
     @Bean
     public RoleHierarchy roleHierarchy() {
         return RoleHierarchyImpl
-            .withDefaultRolePrefix()
-            .role("ADMINISTRATOR").implies("GUIDE")
-            .role("GUIDE").implies("CUSTOMER")
-            .build();
+                .withDefaultRolePrefix()
+                .role("ADMINISTRATOR").implies("GUIDE")
+                .role("GUIDE").implies("CUSTOMER")
+                .build();
     }
 
     @Bean
@@ -61,30 +151,32 @@ public class JwtWebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .cors(corsCustomizer ->
-                corsCustomizer.configurationSource(corsConfigurationSource())
-            )
-            .headers(headers -> headers
-                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
-            )
-            .authorizeHttpRequests(authorizeHttpRequestsCustomizer ->
-                authorizeHttpRequestsCustomizer
-                    .requestMatchers(
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/h2-console/**",
-                        "/api/user/register",
-                        "/api/user/login"
-                    )
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
-            )
-            .sessionManagement(sessionManagementConfigurer ->
-                sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(corsCustomizer ->
+                        corsCustomizer.configurationSource(corsConfigurationSource())
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .authorizeHttpRequests(authorizeHttpRequestsCustomizer ->
+                        authorizeHttpRequestsCustomizer
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/api/swagger-ui/**",
+                                        "/api/v3/api-docs/**",
+                                        "/h2-console/**",
+                                        "/api/user/register",
+                                        "/api/user/login"
+                                )
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
